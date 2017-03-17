9a0a6e27a1cced210c2c262d806bf5a44deecaa8
Chasemorley <rita@chasemorley.com>
2017-03-17 16:48:09 +0000

    now to create CSS for mobile devices.. still some problems

--- js/scroll.js
+++ js/scroll.js
@@ -5,11 +5,16 @@ function Scroll(){
   if( yPos< 200){
     var slowscroll = 800 - (yPos*100)
     header.style.height = "" + slowscroll +"px ";
+    header.style.background = "rgba(250,0,0,0.0)"
+
   }
 
   if(yPos > 200 ){
     header.style.height = "200px"
+    header.style.background = "rgba(250,0,0,0.6)"
+
   }
+
   else {
     header.style.height = "800px"
   }
