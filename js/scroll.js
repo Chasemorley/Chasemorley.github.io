15d5cab4801868016164c8ec29f47d770de65d8d
Chasemorley <rita@chasemorley.com>
2017-03-17 15:19:06 +0000

    fixed header problem

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,16 @@
-var = header, yPos;
-function yScroll(){
-  header = document.getElementById('header')
-  yPos = window.pageYOffset;
-  if(yPos > 11){
-    header.style.height = header.style.height - yPos;
-  } else {
+
+function Scroll(){
+  var header = document.getElementById('header')
+  var yPos = window.pageYOffset;
+  if(yPos > 1, yPos< 300){
+    var slowscroll = parseInt(header.style.height,10) - (yPos*100)
+    header.style.height = "" + slowscroll +"px ";
+  }
+  if(yPos > 300 )
+    header.style.height = "200px"
+  else {
     header.style.height = "800px"
   }
 }
 
-window.addEventListener("scroll", yScroll)
+window.addEventListener("scroll", Scroll)
