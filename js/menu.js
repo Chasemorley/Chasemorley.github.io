e4c2ce4f570f8735fc99d646451012875b5b6c96
Chasemorley <rita@chasemorley.com>
2017-03-19 14:51:13 +0000

    created the menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,9 @@
 $(function(){
   $(".mobile-nav").click(function(){
       $(this).toggleClass("visible");
+      $(".site-nav").toggleClass("visible")
+      $(".header").toggleClass("invisible")
+      $(".contentarea").toggleClass("invisible")
   })
 
 })
