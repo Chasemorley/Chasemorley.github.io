84670323869fc7888a9ba0ccca545461978d1f18
Chasemorley <rita@chasemorley.com>
2017-03-20 13:18:35 +0000

    looking good need to add text etc

--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,30 @@ $(function(){
   })
 
 })
+
+$(function(){
+  $(".menubutton").click(function(){
+    $(".site-nav").toggleClass("visible")
+    $(".mobile-nav").toggleClass("visible")
+    $(".companytitleandslogan").toggleClass("invisible")
+    $(".contentarea").toggleClass("invisible")
+
+  })
+})
+
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
